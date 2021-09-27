bc57033df108d25ffca078b24da1efbdff855dd0
ryangarza <87188488+ryangarza@users.noreply.github.com>
2021-09-27 12:38:06 -0500

    Add files via upload

---
+++ CODE/DataExploration.R
@@ -0,0 +1,7 @@
+##-------Import the Dataset----------
+
+library(readr)
+adult_all <- read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/adult-all.csv")
+View(adult_all) 
+
+##------Rename Variable Columns-----------
